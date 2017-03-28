2e317f2b09542b201cbb06ac794551174d45daf2
neil4droid <cterse@gmail.com>
2017-03-28 22:15:46 +0530

    EXPT06

---
+++ HMIPrac06-Multimedia/home.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Best Of Coke Studio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="home.css">
+
+</head>
+<body>
+  
+<header>
+  <div class="container header inner">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Best of Coke Studio</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav navbar-right masthead-nav">
+            <li><a href="#"> Season 1</a></li>
+            <li><a href="#"> Season 2</a></li>
+            <li><a href="#"> Season 3</a></li>
+            <li><a href="#"> Season 4</a></li>
+          </ul>
+        </div>
+      </div>
+      </nav>
+  </div>
+</header>
+
+<div class="container" style="margin-top: 20px;margin-bottom: 20px;">
+  
+  <div class="row">
+    <div class="col-sm-8">
+      <h3 style="color:;">Afreen Afreen, Rahat Fateh Ali Khan & Momina Mustehsan:</h3>
+      <video src="E:\Chinmay\HMI\HMIPrac06-Multimedia\afreen.mp4" controls autoplay="true"></video>
+
+      <div>
+        <h3 style="color: ;">Audio Only:</h3>
+
+        <div class="btn-group" style="">
+          <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-fast-backward"></span></button>
+          <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-backward"></span></button>
+          <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-play"></span></button>
+          <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-pause"></span></button>
+          <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-stop"></span></button>
+          <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-forward"></span></button>
+          <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-fast-forward"></span></button>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <h3 class="text-center" style="color: ">Video Info</h3>
+      <p class="text-center" style="color: ">Published on Aug 19, 2016
+Nusrat Fateh Ali Khan was one of the shining lights of Pakistan, a Qawwali Shahehshah whose talent was heralded both locally and globally. To celebrate the legend that still lives 19 years on, Coke Studio presents
+Afreen Afreen, Rahat Fateh Ali Khan and Momina Mustehsan, Episode 2, Coke Studio 9
+Music Directed by Faakhir Mehmood
+Produced by Strings
+The beautifully crafted ‘Afreen Afreen’ sung two decades ago by the legendry, Nusrat Fateh Ali Khan still holds the strength to serenade the audience with its poetic and soothing sensibilities. The reformation of the track on Coke Studio – arranged by Faakhir Mehmood and performed by maestro Rahat Fateh Ali Khan and debutant, Momina Mustehsan – transforms the Qawwali seamlessly into an unplugged track. Within the genre, the atmosphere of the song remains ambient and acoustic, as the bare minimum elements give room to Rahat’s majestic vocals to improvise the mastery he possesses. Momina enters the song as she punctuates the additional melody with her laid-back effortless velvety vocals. Towards the end, the track is perfectly complimented with the incredible drum roll and a hyper groove as the traditional ‘Afreen Afreen’ chants are delivered with heartfelt emotion and melodic panache.
+#CokeStudio9</p>
+    </div>
+  </div>
+</div>
+
+
+</body>
+</html>
